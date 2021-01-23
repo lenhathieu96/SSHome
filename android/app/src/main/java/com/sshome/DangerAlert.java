bc9cdf87826e7f5ad9331de6f24157facbb9c7bf
Lê Nhật Hiếu <LeeNhaHie@Macbooks-MacBook-Pro.local>
2021-01-23 11:29:40 +0700

    add alert notify_android

--- android/app/src/main/java/com/sshome/DangerAlert.java
+++ android/app/src/main/java/com/sshome/DangerAlert.java
@@ -7,7 +7,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -16,7 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.facebook.react.ReactActivity;
 
 public class DangerAlert extends ReactActivity {
-    private Ringtone ringtone;
+    Button btn_call;
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,13 +27,22 @@ public class DangerAlert extends ReactActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
         setContentView(R.layout.danger_alert);
-        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        this.ringtone = RingtoneManager.getRingtone(getApplicationContext(),alert);
+        Uri alert = Uri.parse("android.resource://"
+                + this.getPackageName() + "/" + R.raw.fire_alert);
+        Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(),alert);
         ringtone.setLooping(true);
         ringtone.setVolume(1);
         ringtone.play();
-        long[] pattern = { 0, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500};
+        long[] pattern = { 0, 1000, 200};
         Vibrator v =  (Vibrator)getSystemService(this.VIBRATOR_SERVICE);
-        v.vibrate(VibrationEffect.createWaveform(pattern, -1));
+        v.vibrate(VibrationEffect.createWaveform(pattern, 0));
+        btn_call = (Button)findViewById(R.id.btn_call);
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             ringtone.stop();
+             v.cancel();
+            }
+        });
     }
 }
